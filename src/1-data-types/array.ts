d0061c45f2562504c3c741173cf8d487bc8e1585
cjrequea <requenacj@gmail.com>
2020-07-14 15:57:30 +0200

    typescript::datatypes::array

--- src/1-data-types/array.ts
+++ src/1-data-types/array.ts
@@ -1,8 +1,10 @@
 // - Arrays in Typescript works in the same way as they work in JavaScript
 // - Same as JavaScript, TypeScript allows us to deal with arrays of values
-// - There are two ways to write/declare an array:
+// - There are several ways to write/declare an array:
 //     1. let cities: string[];
 //         - let arrFrameworks: string [] = ['JavaScript','jQuery','Angular','React'];
+//         - let arrFrameworks2: [string, string, string, string] = ["JavaScript","jQuery","Angular","React"];
+
 //     2. let cities: Array<string>
 //         - let arrLanguages: Array<string> = ['C','C++','Java','Ruby','Phthon'];
 //         - In the above example, we declared an array of strings by assigning it the string type (Now TypeScript will make sure the array contains only strings)
@@ -10,7 +12,10 @@
 (() => {
   console.log("typescript::datatypes::array");
   let arrFrameworks: string[] = ["JavaScript", "jQuery", "Angular", "React"];
+  let arrFrameworks2: [string, string, string, string, number?] = ["JavaScript","jQuery","Angular","React"];
+
   console.log("arrFrameworks:", arrFrameworks);
+  console.log("arrFrameworks2:", arrFrameworks2);
 
   let arrLanguages: Array<string> = ["C", "C++", "Java", "Ruby", "Phthon"];
   console.log("arrLanguages", arrLanguages);
